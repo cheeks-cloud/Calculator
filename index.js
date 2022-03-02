4f81127500f0791798eeb73a459468732cfda3d1
@cheeks cloud <agneskaswii700@gmail.com>
2022-03-02 20:33:17 +0300

    creating functions for subtracting,adding and multiplying

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ var add = (number1, number2) => {
   };
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
+  var result1 = add(number1, number2);
   
-  alert(add( number1, number2));
+  alert(result1);
   
    var subtract = (number1,number2) =>{
-  return number1 - number2;
-  };
-  alert(subtract(45,23)); 
+     return number1 - number2;
+     };
+     var number1 = parseInt(prompt("Enter a number:"));
+     var number2 = parseInt(prompt("Enter another number:"));
+     var result2 = subtract(number1, number2);
+
+    alert(result2); 
+
+    var multiply = (number1,number2) =>{
+      return number1 * number2;
+      };
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+      var result3 = subtract(number1, number2);
+ 
+     alert(result3);
+
+     var divide = (number1,number2) =>{
+      return number1 / number2;
+      };
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+      var result4 = subtract(number1, number2);
+ 
+     alert(result4); 

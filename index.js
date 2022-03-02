c84fbb7ab02aaa99ed544ff8b990dd9f94a24fcf
@cheeks cloud <agneskaswii700@gmail.com>
2022-03-02 20:15:52 +0300

    creating the initial folders and addition function

---
+++ index.js
@@ -0,0 +1,12 @@
+var add = (number1, number2) => {
+  return number1 + number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  
+  alert(add( number1, number2));
+  
+   var subtract = (number1,number2) =>{
+  return number1 - number2;
+  };
+  alert(subtract(45,23)); 

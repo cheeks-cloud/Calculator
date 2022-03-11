7727994677a8e6901a1e51d4720708175c72dae1
@cheeks cloud <agneskaswii700@gmail.com>
2022-03-11 08:32:48 +0300

    created form to take input and output after a function

--- index.js
+++ index.js
@@ -1,35 +1,100 @@
-var add = (number1, number2) => {
-  return number1 + number2;
-  };
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result1 = add(number1, number2);
-  
-  alert(result1);
-  
+
+  var add = (number1, number2) => {
+     return number1 + number2;
+   };
    var subtract = (number1,number2) =>{
      return number1 - number2;
      };
-     var number1 = parseInt(prompt("Enter a number:"));
-     var number2 = parseInt(prompt("Enter another number:"));
-     var result2 = subtract(number1, number2);
-
-    alert(result2); 
-
+     
     var multiply = (number1,number2) =>{
       return number1 * number2;
       };
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-      var result3 = subtract(number1, number2);
- 
-     alert(result3);
-
+    
      var divide = (number1,number2) =>{
       return number1 / number2;
       };
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-      var result4 = subtract(number1, number2);
- 
-     alert(result4); 
+    
+
+ $(document).ready(function(){
+   $("form#add").submit(function(event){
+     event.preventDefault()
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1,number2)
+    $("#output").text(result)
+  })
+ })
+ $(document).ready(function(){
+  $("form#add").submit(function(event){
+    event.preventDefault()
+   var number1 = parseInt($("#add1").val());
+   var number2 = parseInt($("#add2").val());
+   var result = subtract(number1,number2)
+   $("#output").text(result)
+ })
+})
+$(document).ready(function(){
+  $("form#add").submit(function(event){
+    event.preventDefault()
+   var number1 = parseInt($("#add1").val());
+   var number2 = parseInt($("#add2").val());
+   var result = Multiply(number1,number2)
+   $("#output").text(result)
+ })
+})
+$(document).ready(function(){
+  $("form#add").submit(function(event){
+    event.preventDefault()
+   var number1 = parseInt($("#add1").val());
+   var number2 = parseInt($("#add2").val());
+   var result = Divide(number1,number2)
+   $("#output").text(result)
+ })
+})
+
+
+
+
+
+
+
+
+
+
+// let initialBalance1 = 1000000
+// let interest1 = 5
+// let initialBalance2 = 8500000
+// let interest2 = 6.6
+
+// let duration  = 10
+// final_balance1 = calculateFinalBalance(initialBalance1,interest1,duration)
+// final_balance2 = calculateFinalBalance(initialBalance2,interest2, duration)
+
+
+// //creating a function to claculate money after compound rate
+// var final_balance1 = (initialBalance1,interest1, noOfYears) =>{
+//   // return balance*interest_rate
+//   let countYears = 0;
+//   while(countYears < noOfYears){
+//     totalbalance = calculateIncreasedBalance(initialBalance1,interest1)
+//     //each time we go thoughy the loop our balance should increase
+//     countYears += 1 //pretend a year has passed
+//   }
+//   return totalbalance
+// }
+// var final_balance2 = (initialBalance2,interest2, noOfYears) =>{
+//   // return balance*interest_rate
+//   let countYears = 0;
+//   while(countYears < noOfYears){
+//     balance = calculateIncreasedBalance(initialBalance2,interest2)
+//     //each time we go thoughy the loop our balance should increase
+//     countYears += 1 //pretend a year has passed
+//   }
+//   return balance
+// }
+// profit1 = final_balance1 - initialBalance1
+// profit2 = final_balance2 - initialBalance2
+
+// if(profit1>profit12){alert("this is the best one")}else{
+//   alert(" investment 2 is better")
+// }
